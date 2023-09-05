065fca06552f82526a7647056e6c8bd509800bc3
Steven Bucher <stevenbucher8@gmail.com>
2023-09-05 12:14:03 -0700

    fixing cmts

--- src/ai.sh
+++
@@ -1,6 +0,0 @@
-
-command_str='bind -x "\"\eOR\":\"dotnet ai.dll --restore\""'
-
-echo "$command_str" >> "$HOME/.inputrc"
-
-echo "Shell Copilot registered for F3. Please restart your terminal or run 'source ~/.inputrc' to apply the changes."
