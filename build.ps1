ec34010cfe3e6f0878b5fed4c226f562d2b7a340
Steven Bucher <stevenbucher8@gmail.com>
2023-10-21 20:41:26 -0700

    first pass for updating readme

--- build.ps1
+++ build.ps1
@@ -9,24 +9,60 @@ param (
 
     [Parameter()]
     [switch]
-    $Clean
+    $Clean,
+
+    [Parameter()]
+    [switch]
+    $ShellCopilot,
+
+    [Parameter()]
+    [switch]
+    $PSCopilot
 )
 
 try {
-    Push-Location "$PSScriptRoot/src/code"
 
-    $outPath = "$PSScriptRoot/out/Microsoft.PowerShell.Copilot"
+    if($ShellCopilot) {
+        Write-Host "Building ShellCopilot..."
+        Push-Location "$PSScriptRoot/shell/ShellCopilot.App"
 
-    if ($Clean) {
-        if (Test-Path $outPath) {
-            Write-Verbose "Deleting $outPath"
-            Remove-Item -recurse -force -path $outPath
+        $outPath = "$PSScriptRoot/out/ShellCopilot.App"
+
+        if ($Clean) {
+            if (Test-Path $outPath) {
+                Write-Verbose "Deleting $outPath"
+                Remove-Item -recurse -force -path $outPath
+            }
+
+            dotnet clean
         }
 
-        dotnet clean
+        dotnet publish --output $outPath --configuration $Configuration
+        Write-Host "ShellCopilot built successfully, output path: $outPath"
     }
 
-    dotnet publish --output $outPath --configuration $Configuration
+    elseif($PSCopilot) {
+        Write-Host "Building PSCopilot..."
+        Push-Location "$PSScriptRoot/src/code"
+
+        $outPath = "$PSScriptRoot/out/Microsoft.PowerShell.Copilot"
+
+        if ($Clean) {
+            if (Test-Path $outPath) {
+                Write-Verbose "Deleting $outPath"
+                Remove-Item -recurse -force -path $outPath
+            }
+
+            dotnet clean
+        }
+
+        dotnet publish --output $outPath --configuration $Configuration
+        Write-Host "ShellCopilot built successfully, output path: $outPath"
+    }
+    else {
+        Write-Host "Please specify which prototype to build with -ShellCopilot or -PSCopilot switch paramters"
+    }
+    
 }
 finally {
     Pop-Location
