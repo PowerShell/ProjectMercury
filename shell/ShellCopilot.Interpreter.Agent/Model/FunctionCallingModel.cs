0bb40c1672f49f6e4995de36517e341fcd2ceb5d
Justin Chung <t-juchung@microsoft.com>
2024-03-13 13:33:49 -0500

    Powershell preferred.

--- shell/ShellCopilot.Interpreter.Agent/Model/FunctionCallingModel.cs
+++ shell/ShellCopilot.Interpreter.Agent/Model/FunctionCallingModel.cs
@@ -20,7 +20,7 @@ static internal class Tools
     static internal ChatCompletionsFunctionToolDefinition RunCode = new()
     {
         Name = "execute",
-        Description = "This function is able to run given powershell and python code. This will allow you to execute powershell and python code " +
+        Description = "This function is able to run given powershell code. This will allow you to execute powershell code " +
         "on my local machine.",
         Parameters = BinaryData.FromObjectAsJson(
         new
