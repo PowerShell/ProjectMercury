0d8e0f2fd9f3c01435cb81585e6755fda7677f22
Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
2024-03-27 13:26:13 +0800

    Support more response status from backend side (#100)

--- shell/ShellCopilot.Azure.Agent/AzPS/AzPSChatService.cs
+++ shell/ShellCopilot.Azure.Agent/AzPS/AzPSChatService.cs
@@ -123,24 +123,14 @@ internal class AzPSChatService : IDisposable
             while ((line = await reader.ReadLineAsync(cancellationToken)) is not null)
             {
                 var chunk = JsonSerializer.Deserialize<ChunkData>(line, Utils.JsonOptions);
-                if (chunk.Status.Equals("Starting Search Examples", StringComparison.Ordinal))
-                {
-                    context?.Status("Searching Examples ...");
-                    continue;
-                }
-
-                if (chunk.Status.Equals("Starting Search Cmdlet Reference", StringComparison.Ordinal))
-                {
-                    context?.Status("Searching Cmdlet Reference ...");
-                    continue;
-                }
-
-                if (chunk.Status.Equals("Starting Generate Answer", StringComparison.Ordinal))
+                if (chunk.Status.Equals("Generating Answer", StringComparison.Ordinal))
                 {
                     // Received the first chunk for the real answer.
                     // Wrap it along with the reader and return the wrapper.
                     return new ChunkReader(reader, chunk);
                 }
+
+                context?.Status(chunk.Status);
             }
         }
         catch (OperationCanceledException)
