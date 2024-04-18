0a0b15f5cb96657e3fee1447a892e6aee3aaee36
Justin Chung <t-juchung@microsoft.com>
2024-04-18 00:31:08 -0500

    fixed python spacing problem.

--- shell/ShellCopilot.Interpreter.Agent/ExecutionService/Languages/Python.cs
+++ shell/ShellCopilot.Interpreter.Agent/ExecutionService/Languages/Python.cs
@@ -7,31 +7,31 @@
 /// </summary>
 internal class Python: SubprocessLanguage
 {
-	internal Python()
-	{
-		// -q doesn't print the banner
-		// -i runs the code in interactive mode
-		// -u unbuffered binary stdout and stderr
-		// Without these flags, the output is buffered and we can't read it until the process ends
-		StartCmd = ["python.exe", "-qui"];
-		VersionCmd = ["python.exe", "-V"];
-		OutputQueue = new Queue<Dictionary<string, string>>();
-	}
+    internal Python()
+    {
+        // -q doesn't print the banner
+        // -i runs the code in interactive mode
+        // -u unbuffered binary stdout and stderr
+        // Without these flags, the output is buffered and we can't read it until the process ends
+        StartCmd = ["python.exe", "-qui"];
+        VersionCmd = ["python.exe", "-V"];
+        OutputQueue = new Queue<Dictionary<string, string>>();
+    }
 
-	protected override string PreprocessCode(string code)
-	{
-		code = code.TrimEnd();
-		code += "\n\nprint('##end_of_execution##')";
+    protected override string PreprocessCode(string code)
+    {
+        code = code.TrimEnd();
+        code += "\n\nprint('##end_of_execution##')";
         return code;
     }
 
-	protected override void WriteToProcess(string code)
-	{
-		// Split the code into lines and send each line to the process
-		List<string> lines = code.Split("\n").ToList();
+    protected override void WriteToProcess(string code)
+    {
+        // Split the code into lines and send each line to the process
+        List<string> lines = code.Split("\n").ToList();
 
-		foreach (string line in lines)
-		{
+        foreach (string line in lines)
+        {
             Process.StandardInput.WriteLine(line);
             Process.StandardInput.Flush();
         }
