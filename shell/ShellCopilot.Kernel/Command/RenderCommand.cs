c660c5563b43db7f64dfe08cefd024407bb2b687
Dongbo Wang <dongbow@microsoft.com>
2024-05-25 22:38:46 -0700

    Improve the rendering of `ListItemBlock` and add the `/render` command (#143)

---
+++ shell/ShellCopilot.Kernel/Command/RenderCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.CommandLine;
+using ShellCopilot.Abstraction;
+
+namespace ShellCopilot.Kernel.Commands;
+
+internal sealed class RenderCommand : CommandBase
+{
+    public RenderCommand()
+        : base("render", "Render a markdown file, for diagnosis purpose.")
+    {
+        var file = new Argument<FileInfo>("file", "The file path to save the code to.");
+        AddArgument(file);
+        this.SetHandler(SaveAction, file);
+    }
+
+    private void SaveAction(FileInfo file)
+    {
+        var host = Shell.Host;
+
+        try
+        {
+            using FileStream stream = file.OpenRead();
+            using StreamReader reader = new(stream, Encoding.Default);
+
+            string text = reader.ReadToEnd();
+            host.RenderFullResponse(text);
+        }
+        catch (Exception e)
+        {
+            host.WriteErrorLine(e.Message);
+        }
+    }
+}
