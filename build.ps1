b9b3776c982762147566eafb9d94533ae04cd3f4
Dongbo Wang <dongbow@microsoft.com>
2024-06-17 15:15:56 -0700

    Rename a few variables in build script (#187)

--- build.ps1
+++ build.ps1
@@ -42,7 +42,7 @@ $app_dir = Join-Path $shell_dir "AIShell.App"
 $pkg_dir = Join-Path $shell_dir "AIShell.Abstraction"
 $module_dir = Join-Path $shell_dir "AIShell.Integration"
 
-$open_ai_agent_dir = Join-Path $agent_dir "AIShell.OpenAI.Agent"
+$openai_agent_dir = Join-Path $agent_dir "AIShell.OpenAI.Agent"
 $interpreter_agent_dir = Join-Path $agent_dir "AIShell.Interpreter.Agent"
 $ollama_agent_dir = Join-Path $agent_dir "AIShell.Ollama.Agent"
 
@@ -51,7 +51,7 @@ $pkg_out_dir = Join-Path $PSScriptRoot "out" "package"
 $app_out_dir = Join-Path $PSScriptRoot "out" $config "app"
 $module_out_dir = Join-Path $PSScriptRoot "out" $config "module" "AIShell"
 
-$open_ai_out_dir = Join-Path $app_out_dir "agents" "AIShell.OpenAI.Agent"
+$openai_out_dir = Join-Path $app_out_dir "agents" "AIShell.OpenAI.Agent"
 $interpreter_out_dir = Join-Path $app_out_dir "agents" "AIShell.Interpreter.Agent"
 $ollama_out_dir =  Join-Path $app_out_dir "agents" "AIShell.Ollama.Agent"
 
@@ -81,8 +81,8 @@ if ($LASTEXITCODE -eq 0) {
 
 if ($LASTEXITCODE -eq 0 -and $AgentToInclude -contains 'openai-gpt') {
     Write-Host "`n[Build the OpenAI agent ...]`n" -ForegroundColor Green
-    $open_ai_csproj = GetProjectFile $open_ai_agent_dir
-    dotnet publish $open_ai_csproj -c $Configuration -o $open_ai_out_dir
+    $openai_csproj = GetProjectFile $openai_agent_dir
+    dotnet publish $openai_csproj -c $Configuration -o $openai_out_dir
 }
 
 if ($LASTEXITCODE -eq 0 -and $AgentToInclude -contains 'interpreter') {
