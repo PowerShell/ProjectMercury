7eb773679058335c2b288486a44f5cee2a480140
Dongbo Wang <dongbow@microsoft.com>
2024-10-30 17:31:51 -0700

    Minor update

--- shell/agents/Microsoft.Azure.Agent/Telemetry.cs
+++ shell/agents/Microsoft.Azure.Agent/Telemetry.cs
@@ -17,10 +17,20 @@ public class AzTrace
     {
         InstallationId = null;
 
+        string azCLIProfilePath, azPSHProfilePath;
         string azureConfigDir = Environment.GetEnvironmentVariable("AZURE_CONFIG_DIR");
         string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        string azCLIProfilePath = Path.Combine(azureConfigDir ?? Path.Combine(userProfile, ".azure"), "azureProfile.json");
-        string azPSHProfilePath = Path.Combine(azureConfigDir ?? Path.Combine(userProfile, ".Azure"), "AzureRmContextSettings.json");
+
+        if (string.IsNullOrEmpty(azureConfigDir))
+        {
+            azCLIProfilePath = Path.Combine(userProfile, ".azure", "azureProfile.json");
+            azPSHProfilePath = Path.Combine(userProfile, ".Azure", "AzureRmContextSettings.json");
+        }
+        else
+        {
+            azCLIProfilePath = Path.Combine(azureConfigDir, "azureProfile.json");
+            azPSHProfilePath = Path.Combine(azureConfigDir, "AzureRmContextSettings.json");
+        }
 
         try
         {
