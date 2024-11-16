6c11ef1ba1b03e1ad5a62c3d60f739ee21c0dad6
Dongbo Wang <dongbow@microsoft.com>
2024-11-16 13:11:09 -0800

    Avoid using env:TEMP which is not cross-plat

--- tools/scripts/script.ps1
+++ tools/scripts/script.ps1
@@ -1,16 +1,5 @@
-# ----------------------------------------------------------------------------------
-#
-# Copyright Microsoft Corporation
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ----------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 param(
     [Parameter(HelpMessage = "Specify this parameter to uninstall AIShell")]
@@ -73,7 +62,7 @@ function Install-AIShellApp {
     }
 
     $fileName = [System.IO.Path]::GetFileName($packageUrl)
-    $tempPath = Join-Path $env:TEMP $fileName
+    $tempPath = Join-Path ([System.IO.Path]::GetTempPath()) $fileName
     if (Test-Path $tempPath) {
         Remove-Item $tempPath -Force -ErrorAction Stop
     }
